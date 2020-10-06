9c1dbd33c6f4db0ee0ce2e1abed3f99fdbbbbdb1
Jason Strunk <jasondstrunk@gmail.com>
2020-10-06 16:03:42 -0700

    Some dependancies, serving public folder

---
+++ Proxy/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require('express-http-proxy');
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.use(express.static(__dirname + '/../Public'));
+
+
+app.use('/proxy', proxy({ target: `http://localhost:${port}`, changeorigin: true}));
+
+
+app.listen(port, () => {
+  console.log(`Proxy server listening at http://localhost:${port}`);
+});
+
+app.get('/', (req, res, next) => {
+  res.send('This is a proxy server.');
+});
