6e159d1ce52be1757836dc07bfd36db29be6e3b0
Jason Strunk <jasondstrunk@gmail.com>
2020-10-07 15:17:28 -0700

    Rendering all four to page.

--- Proxy/server.js
+++ Proxy/server.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const proxy = require('express-http-proxy');
+const cors = require('cors');
 
 const app = express();
 const port = 5000;
 
+const whitelist = [
+  'http://localhost:5000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:3003', 'http://localhost:3004'
+];
+const corsOptions = {
+  origin: (origin, cb) => {
+    let originCheck = whitelist.indexOf(origin) !== -1;
+    cb(null, originCheck);
+  },
+  credentials: true
+}
+
+
+
 app.use(express.json());
 app.use(express.static(__dirname + '/../Public'));
 
+app.use(cors(corsOptions));
 
 app.use('/proxy', proxy({ target: `http://localhost:${port}`, changeorigin: true}));
 
 
 app.listen(port, () => {
   console.log(`Proxy server listening at http://localhost:${port}`);
-});
-
-app.get('/', (req, res, next) => {
-  res.send('This is a proxy server.');
 });
