be59b28c5594c68a17e9ba600d4156ce53ca5b21
Jason Strunk <jasondstrunk@gmail.com>
2020-10-08 09:37:37 -0700

    Proxy Server Complete

--- Proxy/server.js
+++ Proxy/server.js
@@ -5,14 +5,8 @@ const proxy = require('express-http-proxy');
 const app = express();
 const port = 5000;
 
-
-
-
-
 app.use(express.json());
 app.use(express.static(__dirname + '/../Public'));
-
-
 
 app.use('/proxy', proxy({ target: `http://localhost:${port}`, changeorigin: true}));
 
